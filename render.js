bd748029c906f4f5f7700ccaeea04683cac40bf1
Jesse Hughes <revelstokesnowboard@gmail.com>
2015-10-08 11:18:26 +0000

    Add Remote location handling.

--- render.js
+++ render.js
@@ -5,45 +5,24 @@ var jade = require('jade')
   , pdf = require('html-pdf')
   , resume;
 
-var args = process.argv.slice(2);
-
 var company,
     url,
     job,
     location = 'Victoria';
 
+var args = process.argv.slice(2);
+
+parseArgs(args);
+
 var templateArgs = {
 
-    job_title: job,
-    application_url: url,
-    company_name: company,
-    location: location
+      job_title: job,
+      application_url: url,
+      company_name: company,
+      location: location
 
     }
 
-args.forEach(function(val, index, array) {
-
-  if ( val === 'company' ) {
-    company = args[index + 1];
-  }
-
-  if ( val === 'url' ) {
-    url = args[index + 1];
-  }
-
-  if ( val === 'job' ) {
-    job = args[index + 1];
-  }
-
-  if ( val === 'locaton' ) {
-    location = args[index + 1];
-  }
-});
-
-if ( !job || !url || !company ){
-  console.log('Required arguments: job url company');
-  process.exit(1);
-}
 
 html = jade.renderFile('resume.jade', templateArgs );
 
@@ -52,3 +31,32 @@ fs.writeFile('./resume.html', html);
 pdf.create(html).toFile('./outgoing/' + company + '/Jesse_Hughes_Resume.pdf', function(err, res){
   console.log('Written to ' + res.filename);
 });
+
+
+function parseArgs (args) {
+
+  args.forEach(function(val, index, array) {
+
+    if ( val === 'company' ) {
+      company = args[index + 1];
+    }
+
+    if ( val === 'url' ) {
+      url = args[index + 1];
+    }
+
+    if ( val === 'job' ) {
+      job = args[index + 1];
+    }
+
+    if ( val === 'location' ) {
+      location = args[index + 1];
+    }
+  });
+
+  if ( !job || !url || !company ){
+    console.log('Required arguments: job url company');
+    process.exit(1);
+  }
+
+}
