4a9c9a5539e9898badaab4ee8ef30f180fe5223e
Jesse Hughes <revelstokesnowboard@gmail.com>
2015-10-08 09:25:15 +0000

    Retab render.js

--- render.js
+++ render.js
@@ -1,46 +1,45 @@
 #! /bin/node
 
 var jade = require('jade')
-	, fs = require('fs')
-	, pdf = require('html-pdf')
-	, resume;
+  , fs = require('fs')
+  , pdf = require('html-pdf')
+  , resume;
 
 var args = process.argv.slice(2);
 
 var company,
-		url,
-		job;
+    url,
+    job;
 
 args.forEach(function(val, index, array) {
 
-	if ( val === 'company' ) {
-		company = args[index + 1];
-	}
+  if ( val === 'company' ) {
+    company = args[index + 1];
+  }
 
-	if ( val === 'url' ) {
-		url = args[index + 1];
-	}
+  if ( val === 'url' ) {
+    url = args[index + 1];
+  }
 
-	if ( val === 'job' ) {
-		job = args[index + 1];
-	}
+  if ( val === 'job' ) {
+    job = args[index + 1];
+  }
 });
 
 if ( !job || !url || !company ){
-	console.log('Required arguments: job url company');
-	process.exit(1);
+  console.log('Required arguments: job url company');
+  process.exit(1);
 }
 
 html = jade.renderFile('resume.jade',
-		{
-			job_title: job,
-			application_url: url,
-			company_name: company
-		});
+    {
+      job_title: job,
+      application_url: url,
+      company_name: company
+    });
 
 fs.writeFile('./resume.html', html);
 
-pdf.create(html).toFile('./outgoing/' + company + '_Resume.pdf', function(err, res){
-	console.log('Written to ' + res.filename);
-	});
-
+pdf.create(html).toFile('./outgoing/' + company + '/Jesse_Hughes_Resume.pdf', function(err, res){
+  console.log('Written to ' + res.filename);
+});
